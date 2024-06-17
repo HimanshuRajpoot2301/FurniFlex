1207aa48033b43e0ced5858b820f603968591606
HimanshuRajpoot2301 <hr3777665@gmail.com>
2024-06-17 15:01:56 +0530

    altered node modules

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,23 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    rollupOptions: {
-      external: ['react-responsive-carousel'],
+  css: {
+    preprocessorOptions: {
+      css: {
+        additionalData: `@import "react-responsive-carousel/lib/styles/carousel.min.css";`
+      },
     },
   },
 });
+
 
 
